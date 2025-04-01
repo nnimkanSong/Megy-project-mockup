0fa57d801fd949b90a7b2525483ba7c185d6ddc1
nnimkanSong <nnimkanc@student.42bangkok.com>
2025-04-02 05:38:46 +0700

    update-nameTitle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'learning-platform',
+  
+  
 }
 
 export default function RootLayout({
